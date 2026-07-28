be8d9a001664f9f1b7c2917696377f8082134ce0
gaon833 <ksh833833@gmail.com>
2026-07-29 04:22:51 +0900

    Refine main layout and project gallery

--- components/project/ProjectGrid.tsx
+++ components/project/ProjectGrid.tsx
@@ -2,11 +2,21 @@ import type { Project } from "@/types/project";
 import ProjectCard from "./ProjectCard";
 
 export default function ProjectGrid({ projects }: { projects: Project[] }) {
+  const leftProjects = projects.filter((_, index) => index % 2 === 0);
+  const rightProjects = projects.filter((_, index) => index % 2 === 1);
+
   return (
     <section className="project-grid">
-      {projects.map((project, index) => (
-        <ProjectCard key={project.id} project={project} index={index} />
-      ))}
+      <div className="project-grid-column">
+        {leftProjects.map((project, index) => (
+          <ProjectCard key={project.id} project={project} index={index * 2} />
+        ))}
+      </div>
+      <div className="project-grid-column">
+        {rightProjects.map((project, index) => (
+          <ProjectCard key={project.id} project={project} index={index * 2 + 1} />
+        ))}
+      </div>
     </section>
   );
 }
